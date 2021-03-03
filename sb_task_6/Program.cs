00c9abfe6d283de2f80f135644036eeda59e8e91
Ivan MSI <arigatory@gmail.com>
2021-03-03 23:36:15 +0300

    Add project files.

---
+++ sb_task_6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace sb_task_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 50;
+            int k = (int) Math.Log2(N);
+            Console.WriteLine($"Считано число N = {N}");
+            Console.WriteLine($"Количество групп: {k+1}");
+            for (int j = 1; j <= k; j++)
+            {
+                int nextK = (int) Math.Pow(2, j);
+                int lim = nextK * 2;
+                for (int i = nextK; i < lim && i <= N; i++)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
+
+
